a371940b8ba7fd2db30340cffe3720372744799e
JaimeGD07 <jaimegildiaz3838@gmail.com>
2023-06-18 00:54:02 -0600

    seleccionarAnioV2.0

--- CanastaBasica/CanastaBasica/InflacionDeCanasta/urls.py
+++ CanastaBasica/CanastaBasica/InflacionDeCanasta/urls.py
@@ -11,11 +11,11 @@ urlpatterns = [
     path('ReporteInflacion',views.reporteInflacion, name='ReporteInflacion'),
     path('iniciarSesion', views.iniciarSesion, name='iniciarSesion'),
     path('registrarse', views.registrarse, name='registrarse'),
-    path('lProductos/2018', views.lProductos, name='lProductos18'),
-    path('lProductos/2019', views.lProductos, name='lProductos19'),
-    path('lProductos/2020', views.lProductos, name='lProductos20'),
-    path('lProductos/2021', views.lProductos, name='lProductos21'),
-    path('lProductos/2022', views.lProductos, name='lProductos'),
+    path('lProductos18', views.lProductos, name='lProductos18'), ##le movi desde aqui hasta el comentario de abajo
+    path('lProductos19', views.lProductos, name='lProductos19'),
+    path('lProductos20', views.lProductos, name='lProductos20'),
+    path('lProductos21', views.lProductos, name='lProductos21'),
+    path('lProductos22', views.lProductos, name='lProductos22'), ##error al realizar el cambio en esta linea (actualizacion, error corregido)
     path('AgregarProducto', views.AgregarProducto, name='AgregarProducto'),
 ]
 
